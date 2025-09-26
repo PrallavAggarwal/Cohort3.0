48d4d99343b0991fbe99558761e6a208b315f833
Prallav Aggarwal <prallavagg002@gmail.com>
2025-09-26 23:43:19 +0530

    updates

--- 15-week/brainly/src/config.ts
+++ 15-week/brainly/src/config.ts
@@ -1,3 +1,3 @@
-export const DBURL = 'mongodb+srv://shinchan1990n:X2FzmD2BlPbD35br@cluster0.zvf7zhv.mongodb.net/brainly'
-export const JWT_SECRET_KEY = 'kyakarogejankar'
+export const DBURL =
+export const JWT_SECRET_KEY =
 
